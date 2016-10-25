1ea0c6a37547b2d824d25796ef48f5f83317b777
Roger Ye <roger.ye@intel.com>
2016-10-25 09:26:09 +0800

    Update test-initrd.sh and test-wifi.sh.

--- scripts/test-wifi.sh
+++ scripts/test-wifi.sh
@@ -2,12 +2,18 @@
 
 . ~/bin/setup.sh
 
-IMG_ROOT=~/vol1/android-6/out/target/product/x86emu
-
 if [ -z "$1" ]; then
-emulator @a23x86 -ranchu -verbose -show-kernel -system $IMG_ROOT/system.img -ramdisk $IMG_ROOT/ramdisk.img -initdata $IMG_ROOT/userdata.img -kernel $IMG_ROOT/kernel -qemu -netdev user,id=mynet1,net=10.0.2.0/24,dhcpstart=10.0.2.50 -device virtio-net,netdev=mynet1
+  EMULATOR1=emulator
 else
-emulator @a23x86 -ranchu -verbose -show-kernel -system $IMG_ROOT/system.img -ramdisk $IMG_ROOT/ramdisk.img -initdata $IMG_ROOT/userdata.img -kernel $IMG_ROOT/kernel
+  EMULATOR1="/opt/VirtualGL/bin/vglrun emulator"
 fi
+
+if [ -z "$OUT" ]; then
+  IMG_ROOT=.
+else
+  IMG_ROOT=$OUT
+fi
+
+$EMULATOR1 @a23x86 -ranchu -verbose -show-kernel -system $IMG_ROOT/system.img -ramdisk $IMG_ROOT/initrd.img -initdata $IMG_ROOT/userdata.img -kernel $IMG_ROOT/kernel -qemu -netdev user,id=mynet1,net=10.0.2.0/24,dhcpstart=10.0.2.50 -device virtio-net,netdev=mynet1
 
 
