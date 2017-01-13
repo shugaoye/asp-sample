fc371b2eae0a19c9afd336931e59fa0a9f8038f5
Roger Ye <roger.ye@intel.com>
2017-01-13 10:06:47 +0800

    Fixed issue in tag-all.sh.

--- scripts/tag-all.sh
+++ scripts/tag-all.sh
@@ -4,51 +4,53 @@ source build/envsetup.sh
 
 tag_android_x86()
 {
-cd bootable/newinstaller; git tag $1; git push github $1; croot
-cd bootable/recovery; git tag $1; git push github $1; croot
-cd device/generic/common; git tag $1; git push github $1; croot
+	echo "Tagging android_x86 projects ..."
+	cd bootable/newinstaller; git tag $1; git push github $1; croot
+	cd bootable/recovery; git tag $1; git push github $1; croot
+	cd device/generic/common; git tag $1; git push github $1; croot
 
-cd bionic; git tag $1; git push github $1; croot
-cd device/generic/firmware; git tag $1; git push github $1; croot
-cd external/alsa-lib; git tag $1; git push github $1; croot
-cd external/alsa-utils; git tag $1; git push github $1; croot
-cd external/bluetooth/bluez; git tag $1; git push github $1; croot
-cd external/bluetooth/glib; git tag $1; git push github $1; croot
-cd external/bluetooth/sbc; git tag $1; git push github $1; croot
-cd external/busybox; git tag $1; git push github $1; croot
-cd external/drm_gralloc; git tag $1; git push github $1; croot
-cd external/drm_hwcomposer; git tag $1; git push github $1; croot
-cd external/e2fsprogs; git tag $1; git push github $1; croot
-cd external/ffmpeg; git tag $1; git push github $1; croot
-cd external/libdrm; git tag $1; git push github $1; croot
-cd external/libpciaccess; git tag $1; git push github $1; croot
-cd external/libtruezip; git tag $1; git push github $1; croot
-cd external/llvm; git tag $1; git push github $1; croot
-cd external/mesa; git tag $1; git push github $1; croot
-cd external/s2tc; git tag $1; git push github $1; croot
-cd external/stagefright-plugins; git tag $1; git push github $1; croot
-cd external/v86d; git tag $1; git push github $1; croot
-cd frameworks/av; git tag $1; git push github $1; croot
-cd frameworks/base; git tag $1; git push github $1; croot
-cd frameworks/native; git tag $1; git push github $1; croot
-cd hardware/broadcom/wlan; git tag $1; git push github $1; croot
-cd hardware/gps; git tag $1; git push github $1; croot
-cd hardware/intel/audio_media; git tag $1; git push github $1; croot
-cd hardware/intel/libsensors; git tag $1; git push github $1; croot
-cd hardware/libaudio; git tag $1; git push github $1; croot
-cd hardware/libcamera; git tag $1; git push github $1; croot
-cd hardware/libhardware; git tag $1; git push github $1; croot
-cd hardware/libhardware_legacy; git tag $1; git push github $1; croot
-cd hardware/liblights; git tag $1; git push github $1; croot
-cd hardware/libsensors; git tag $1; git push github $1; croot
-cd hardware/ril; git tag $1; git push github $1; croot
-cd hardware/x86power; git tag $1; git push github $1; croot
-cd system/core; git tag $1; git push github $1; croot
+	cd bionic; git tag $1; git push github $1; croot
+	cd device/generic/firmware; git tag $1; git push github $1; croot
+	cd external/alsa-lib; git tag $1; git push github $1; croot
+	cd external/alsa-utils; git tag $1; git push github $1; croot
+	cd external/bluetooth/bluez; git tag $1; git push github $1; croot
+	cd external/bluetooth/glib; git tag $1; git push github $1; croot
+	cd external/bluetooth/sbc; git tag $1; git push github $1; croot
+	cd external/busybox; git tag $1; git push github $1; croot
+	cd external/drm_gralloc; git tag $1; git push github $1; croot
+	cd external/drm_hwcomposer; git tag $1; git push github $1; croot
+	cd external/e2fsprogs; git tag $1; git push github $1; croot
+	cd external/ffmpeg; git tag $1; git push github $1; croot
+	cd external/libdrm; git tag $1; git push github $1; croot
+	cd external/libpciaccess; git tag $1; git push github $1; croot
+	cd external/libtruezip; git tag $1; git push github $1; croot
+	cd external/llvm; git tag $1; git push github $1; croot
+	cd external/mesa; git tag $1; git push github $1; croot
+	cd external/s2tc; git tag $1; git push github $1; croot
+	cd external/stagefright-plugins; git tag $1; git push github $1; croot
+	cd external/v86d; git tag $1; git push github $1; croot
+	cd frameworks/av; git tag $1; git push github $1; croot
+	cd frameworks/base; git tag $1; git push github $1; croot
+	cd frameworks/native; git tag $1; git push github $1; croot
+	cd hardware/broadcom/wlan; git tag $1; git push github $1; croot
+	cd hardware/gps; git tag $1; git push github $1; croot
+	cd hardware/intel/audio_media; git tag $1; git push github $1; croot
+	cd hardware/intel/libsensors; git tag $1; git push github $1; croot
+	cd hardware/libaudio; git tag $1; git push github $1; croot
+	cd hardware/libcamera; git tag $1; git push github $1; croot
+	cd hardware/libhardware; git tag $1; git push github $1; croot
+	cd hardware/libhardware_legacy; git tag $1; git push github $1; croot
+	cd hardware/liblights; git tag $1; git push github $1; croot
+	cd hardware/libsensors; git tag $1; git push github $1; croot
+	cd hardware/ril; git tag $1; git push github $1; croot
+	cd hardware/x86power; git tag $1; git push github $1; croot
+	cd system/core; git tag $1; git push github $1; croot
 }
 
 tag_goldfish()
 {
-cd device/generic/goldfish; git tag $1; git push github $1; croot
+	echo "Tagging device/generic/goldfish ..."
+	cd device/generic/goldfish; git tag $1; git push github $1; croot
 }
 
 case $1 in
@@ -56,8 +58,8 @@ case $1 in
 		if [ -d device/generic/x86vbox ]; then
 			echo Tagging x86vbox ...
 			cd device/generic/x86vbox; git tag $1; git push github $1; croot
-			tag_goldfish
-			tag_android_x86
+			tag_goldfish $1
+			tag_android_x86 $1
 		else
 			echo "Cannot find x86vbox."
 			exit
@@ -66,7 +68,7 @@ case $1 in
 	android-6.0.1_r61_x86emu*)
 		if [ -d device/generic/x86emu ]; then
 			echo Tagging x86emu ...
-			tag_goldfish
+			tag_goldfish $1
 			cd device/generic/x86emu; git tag $1; git push github $1; croot
 		else
 			echo "Cannot find x86emu."
