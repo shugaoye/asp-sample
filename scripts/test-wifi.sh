1c75776f39eb6e58ac4e5f1a44f98cd0f99bb5c8
Roger Ye <shugaoye@yahoo.com>
2017-05-17 14:57:05 +0800

    Changed scripts/test-wifi.sh

--- scripts/test-wifi.sh
+++ scripts/test-wifi.sh
@@ -14,6 +14,6 @@ else
   IMG_ROOT=$OUT
 fi
 
-$EMULATOR1 @a23x86 -ranchu -verbose -show-kernel -system $IMG_ROOT/system.img -ramdisk $IMG_ROOT/initrd.img -initdata $IMG_ROOT/userdata.img -kernel $IMG_ROOT/kernel -qemu -netdev user,id=mynet1,net=10.0.2.0/24,dhcpstart=10.0.2.50 -device virtio-net,netdev=mynet1
+$EMULATOR1 @a25x86 -ranchu -verbose -gpu swiftshader -show-kernel -system $IMG_ROOT/system.img -ramdisk $IMG_ROOT/initrd.img -initdata $IMG_ROOT/userdata.img -kernel $IMG_ROOT/kernel -qemu -netdev user,id=mynet1,net=10.0.2.0/24,dhcpstart=10.0.2.50 -device virtio-net,netdev=mynet1
 
 
